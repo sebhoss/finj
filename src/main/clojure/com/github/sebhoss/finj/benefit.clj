5220145826ef17d50ecfc9483660f91ee98a1671
Sebastian Hoß <mail@shoss.de>
2013-06-08 20:34:55 +0200

    More benefit-functions
    Signed-off-by: Sebastian Hoß <mail@shoss.de>

--- src/main/clojure/com/github/sebhoss/finj/benefit.clj
+++ src/main/clojure/com/github/sebhoss/finj/benefit.clj
@@ -13,3 +13,34 @@
   (* payment
      (/ (dec (pow accumulation-factor period))
         (dec accumulation-factor))))
+
+(defn present-due-value [& {:keys [payment accumulation-factor period]}]
+  (* (/ payment
+        (pow accumulation-factor (dec period)))
+     (/ (dec (pow accumulation-factor period))
+        (dec accumulation-factor))))
+
+(defn present-immediate-value [& {:keys [payment accumulation-factor period]}]
+  (* (/ payment
+        (pow accumulation-factor period))
+     (/ (dec (pow accumulation-factor period))
+        (dec accumulation-factor))))
+
+(defn perpetuity-due-value [& {:keys [payment accumulation-factor]}]
+  (/ (* payment accumulation-factor)
+     (dec accumulation-factor)))
+
+(defn perpetuity-immediate-value [& {:keys [payment accumulation-factor]}]
+  (/ payment (dec accumulation-factor)))
+
+(defn period [& {:keys [payment accumulation-factor final-immediate-value present-immediate-value]
+                 :or {final-immediate-value nil
+                      present-immediate-value nil}}]
+  {:pre [(or (not-nil? final-immediate-value)
+             (not-nil? present-immediate-value))]}
+  (if (nil? final-immediate-value)
+    (* (/ 1 (ln accumulation-factor))
+       (ln (/ payment
+              (- payment (* present-immediate-value (dec accumulation-factor))))))
+    (* (/ 1 (ln accumulation-factor))
+       (ln (inc (* final-immediate-value (/ (dec accumulation-factor) payment)))))))
