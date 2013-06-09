9123fbc3054d9253ee09e288ffe96a2fadb01379
Sebastian Hoß <mail@shoss.de>
2013-06-09 23:32:23 +0200

    Replaced map destructuring with defnk-call
    Signed-off-by: Sebastian Hoß <mail@shoss.de>

--- src/main/clojure/com/github/sebhoss/finj/periodic_payment.clj
+++ src/main/clojure/com/github/sebhoss/finj/periodic_payment.clj
@@ -1,9 +1,10 @@
-(ns com.github.sebhoss.finj.periodic-payment)
+(ns com.github.sebhoss.finj.periodic-payment
+  (:require [com.github.sebhoss.finj.def :refer :all]))
 
-(defn due-payments [& {:keys [amount rate period]}]
+(defnk due-payments [:amount :rate :period]
   (* amount (+ period
                (* rate (/ (inc period) 2)))))
 
-(defn immediate-payments [& {:keys [amount rate period]}]
+(defnk immediate-payments [:amount :rate :period]
   (* amount (+ period
                (* rate (/ (dec period) 2)))))
--- src/main/clojure/com/github/sebhoss/finj/repayment.clj
+++ src/main/clojure/com/github/sebhoss/finj/repayment.clj
@@ -1,29 +1,30 @@
 (ns com.github.sebhoss.finj.repayment
-  (:require [com.github.sebhoss.finj.math :refer :all]))
+  (:require [com.github.sebhoss.finj.math :refer :all]
+            [com.github.sebhoss.finj.def :refer :all]))
 
-(defn rate-balance-due [& {:keys [loan period repayment-period]}]
+(defnk rate-balance-due [:loan :period :repayment-period]
   (* loan (- 1 (/ period repayment-period))))
 
-(defn rate-interest-amount [& {:keys [loan period repayment-period rate]}]
+(defnk rate-interest-amount [:loan :period :repayment-period :rate]
   (* loan rate (- 1 (/ (dec period) repayment-period))))
 
-(defn annuity [& {:keys [loan repayment-period accumulation-factor]}]
+(defnk annuity [:loan :repayment-period :accumulation-factor]
   (* loan (/ (* (pow accumulation-factor repayment-period) (dec accumulation-factor))
              (dec (pow accumulation-factor repayment-period)))))
 
-(defn annuity-amount [& {:keys [loan annuity period repayment-period accumulation-factor]}]
+(defnk annuity-amount [:loan :annuity :period :repayment-period :accumulation-factor]
   (* (- annuity (* loan (dec accumulation-factor)))
      (pow accumulation-factor (dec period))))
 
-(defn annuity-balance-due [& {:keys [loan annuity period accumulation-factor]}]
+(defnk annuity-balance-due [:loan :annuity :period :accumulation-factor]
   (- (* loan (pow accumulation-factor period))
      (* annuity (/ (dec (pow accumulation-factor period))
                    (dec accumulation-factor)))))
 
-(defn annuity-interest-amount [& {:keys [annuity first-annuity-amount period accumulation-factor]}]
+(defnk annuity-interest-amount [:annuity :first-annuity-amount :period :accumulation-factor]
   (- annuity (* first-annuity-amount (pow accumulation-factor (dec period)))))
 
-(defn period [& {:keys [loan annuity accumulation-factor]}]
+(defnk period [:loan :annuity :accumulation-factor]
   (* (/ 1 (ln accumulation-factor))
      (- (ln annuity)
         (ln (- annuity (* loan (dec accumulation-factor)))))))
