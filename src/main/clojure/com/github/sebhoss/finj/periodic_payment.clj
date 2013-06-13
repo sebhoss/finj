4a483e4158da46694893fa5927472eec3b54d53f
Sebastian Hoß <mail@shoss.de>
2013-06-13 15:23:02 +0200

    Added documentation for the periodic-payment/ namespace
    Signed-off-by: Sebastian Hoß <mail@shoss.de>

--- src/main/clojure/com/github/sebhoss/finj/periodic_payment.clj
+++ src/main/clojure/com/github/sebhoss/finj/periodic_payment.clj
@@ -1,10 +1,41 @@
 (ns com.github.sebhoss.finj.periodic-payment
+  ""
   (:require [com.github.sebhoss.finj.def :refer :all]))
 
-(defnk due-payments [:amount :rate :period]
+(defnk due-value
+  "Calculates the due value of a sequence of periodic payments.
+
+   Parameters:
+     * amount - The payment per period
+     * rate   - The rate of interest
+     * period - The number of payment periods
+
+   Examples:
+     * (due-value :amount 100 :rate 0.05 :period 5)
+     * (due-value :amount 150 :rate 0.1 :period 8)
+     * (due-value :amount 180 :rate 0.15 :period 12)"
+  [:amount :rate :period]
+  {:pre [(number? amount)
+         (number? rate)
+         (number? period)]}
   (* amount (+ period
                (* rate (/ (inc period) 2)))))
 
-(defnk immediate-payments [:amount :rate :period]
+(defnk immediate-value
+  "Calculates the due value of a sequence of periodic payments.
+
+   Parameters:
+     * amount - The payment per period
+     * rate   - The rate of interest
+     * period - The number of payment periods
+
+   Examples:
+     * (immediate-value :amount 100 :rate 0.05 :period 5)
+     * (immediate-value :amount 150 :rate 0.1 :period 8)
+     * (immediate-value :amount 180 :rate 0.15 :period 12)"
+  [:amount :rate :period]
+  {:pre [(number? amount)
+         (number? rate)
+         (number? period)]}
   (* amount (+ period
                (* rate (/ (dec period) 2)))))
--- src/test/clojure/com/github/sebhoss/finj/periodic_payment_test.clj
+++ src/test/clojure/com/github/sebhoss/finj/periodic_payment_test.clj
@@ -3,10 +3,10 @@
             [com.github.sebhoss.finj.predicate :refer :all]
             [clojure.test :refer :all]))
 
-(deftest due-payments-test
+(deftest due-value-test
   (testing "with positive values"
-    (is (≈ 320 (due-payments :amount 100 :rate 0.10 :period 3)))))
+    (is (≈ 320 (due-value :amount 100 :rate 0.10 :period 3)))))
 
-(deftest immediate-payments-test
+(deftest immediate-value-test
   (testing "with positive values"
-    (is (≈ 310 (immediate-payments :amount 100 :rate 0.10 :period 3)))))
+    (is (≈ 310 (immediate-value :amount 100 :rate 0.10 :period 3)))))
