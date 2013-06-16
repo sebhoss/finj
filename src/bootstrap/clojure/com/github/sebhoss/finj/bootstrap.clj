9bfbf857d2b14d322d756bffdd76f1baf788fb8b
Sebastian Hoß <mail@shoss.de>
2013-06-17 01:11:07 +0200

    Added import from clojure.repl for 'doc', etc. functions.
    Signed-off-by: Sebastian Hoß <mail@shoss.de>

--- src/bootstrap/clojure/com/github/sebhoss/finj/bootstrap.clj
+++ src/bootstrap/clojure/com/github/sebhoss/finj/bootstrap.clj
@@ -13,9 +13,16 @@
 (require '[com.github.sebhoss.finj.root-finding :as root-finding])
 (require '[com.github.sebhoss.finj.share-price :as share-price])
 
+(require '[clojure.repl :refer :all])
 (require '[clojure.test :as test])
 
 (defn rat
+  "[R]uns-[A]ll-[T]ests inside the whole project or in a separate namespace.
+
+   Examples:
+     * (rat)          - Run all tests in all project namespaces
+     * (rat \"common\") - Run all tests in the common/-namespace
+     * (rat \"math\")   - Run all tests in the math/-namespace"
   ([] (test/run-all-tests #"com.github.sebhoss.finj.*-test"))
   ([namespace] (test/run-all-tests
                  (re-pattern (format "com.github.sebhoss.finj.%s-test"
