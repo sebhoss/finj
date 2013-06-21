3a822e3a4bdda991a352cf3791b8395ee1f559d9
Sebastian Hoß <mail@shoss.de>
2013-06-21 16:29:13 +0200

    Added first test for the 'deprecation' namespace.
    More to follow...
    
    Signed-off-by: Sebastian Hoß <mail@shoss.de>

--- src/test/clojure/com/github/sebhoss/finj/deprecation_test.clj
+++ src/test/clojure/com/github/sebhoss/finj/deprecation_test.clj
@@ -1,2 +1,10 @@
-(ns com.github.sebhoss.finj.deprecation-test)
+(ns com.github.sebhoss.finj.deprecation-test
+  (:require [com.github.sebhoss.finj.deprecation :refer :all]
+            [com.github.sebhoss.finj.predicate :refer :all]
+            [clojure.test :refer :all]))
 
+(deftest straight-line-annual-expense-test
+  (testing "with positive integers"
+    (is (= 180 (straight-line-annual-expense :fixed-asset 1000 :residual-value 100 :period 5)))
+    (is (= 225 (straight-line-annual-expense :fixed-asset 2000 :residual-value 200 :period 8)))
+    (is (= 225 (straight-line-annual-expense :fixed-asset 3000 :residual-value 300 :period 12)))))
