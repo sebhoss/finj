e79487060a56d9373f187b5c7d0c92f1163e6d6b
Sebastian Hoß <mail@shoss.de>
2013-07-08 17:49:26 +0200

    Added license header
    Signed-off-by: Sebastian Hoß <mail@shoss.de>

--- src/main/clojure/com/github/sebhoss/finj/compound_interest.clj
+++ src/main/clojure/com/github/sebhoss/finj/compound_interest.clj
@@ -1,3 +1,8 @@
+;   This program is free software. It comes without any warranty, to the extent permitted by applicable law.
+;   You can redistribute it and/or modify it under the terms of the Do What The Fuck You Want To Public
+;   License, Version 2, as published by Sam Hocevar. See "http://www.wtfpl.net/":http://www.wtfpl.net/ for
+;   more details.
+
 (ns com.github.sebhoss.finj.compound-interest
   "Compound interest arises when interest is added to the principal, so that, from that moment on, the interest that
    has been added also earns interest. This addition of interest to the principal is called compounding.
--- src/main/clojure/com/github/sebhoss/finj/investment.clj
+++ src/main/clojure/com/github/sebhoss/finj/investment.clj
@@ -1,3 +1,8 @@
+;   This program is free software. It comes without any warranty, to the extent permitted by applicable law.
+;   You can redistribute it and/or modify it under the terms of the Do What The Fuck You Want To Public
+;   License, Version 2, as published by Sam Hocevar. See "http://www.wtfpl.net/":http://www.wtfpl.net/ for
+;   more details.
+
 (ns com.github.sebhoss.finj.investment
   "In finance, investment is putting money into something with the expectation of gain, usually over a longer term.
    This may or may not be backed by research and analysis. Most or all forms of investment involve some form of risk,
