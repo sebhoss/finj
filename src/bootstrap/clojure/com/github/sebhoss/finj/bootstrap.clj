0c9075fd4ceb0065178500d2d189b0c8419b4090
Sebastian Hoß <mail@shoss.de>
2013-07-10 16:37:30 +0200

    Reworked bootstrap script to use the new bootstrap-clj.
    It's shorter & offers more features.
    
    Signed-off-by: Sebastian Hoß <mail@shoss.de>

--- src/bootstrap/clojure/com/github/sebhoss/finj/bootstrap.clj
+++ src/bootstrap/clojure/com/github/sebhoss/finj/bootstrap.clj
@@ -2,51 +2,14 @@
 ;   You can redistribute it and/or modify it under the terms of the Do What The Fuck You Want To Public
 ;   License, Version 2, as published by Sam Hocevar. See http://www.wtfpl.net/ for more details.
 
-;;; Load all finj namespaces
-;; 0. Require dependencies
-(require '[clojure.java.classpath :refer [classpath]])
-(require '[clojure.tools.namespace.find :refer [find-namespaces]])
-(require '[clojure.string :refer [blank? split]])
+(require '[com.github.sebhoss.bootstrap.repl :refer :all])
 
-;; 1. Find all namespaces inside this project and all dependencies on the classpath
-(defn finj-namespace? [namespace]
-  (not (blank? (re-find #".*?finj" (.toString namespace)))))
+; Load project namespaces
+(load-namespaces #".*?finj")
 
-(def finj-namespaces
-  (find-namespaces (filter finj-namespace? (classpath))))
+; 'clojure.test', 'clojure.repl' and 'clojure.tools.namespace.repl' support
+(load-helpers)
 
-;; 2. Require every namespace with its name as alias
-(defn ns-alias [namespace]
-  (symbol (last (split (.toString namespace) #"\."))))
-
-(defn ns-alias-split [namespace]
-  (vector namespace (ns-alias namespace)))
-
-(def finj-namespace-alias
-  (map ns-alias-split finj-namespaces))
-
-(defn require-namespace [[namespace alias]]
-  (require (vector namespace :as alias)))
-
-(defn load-finj-namespaces []
-  (when (empty? (remove nil?
-                  (map require-namespace finj-namespace-alias)))
-      :ok))
-
-;; 3. Load finj namespaces (+ create alias for load-finj-namespaces)
-(def lfn (load-finj-namespaces))
-
-
-;;; 'refresh' support
-(require '[clojure.tools.namespace.repl :refer [refresh]])
-
-
-;;; 'doc', 'source', etc. support
-(require '[clojure.repl :refer :all])
-
-
-;;; Alias function which runs all tests inside the finj project
-(require '[clojure.test :as test])
 
 (defn rat
   "[R]uns-[A]ll-[T]ests inside the whole project or in a separate namespace.
@@ -55,7 +18,7 @@
      * (rat)          - Run all tests in all project namespaces
      * (rat \"common\") - Run all tests in the common/-namespace
      * (rat \"math\")   - Run all tests in the math/-namespace"
-  ([] (test/run-all-tests #"com.github.sebhoss.finj.*-test"))
-  ([namespace] (test/run-all-tests
+  ([] (run-all-tests #"com.github.sebhoss.finj.*-test"))
+  ([namespace] (run-all-tests
                  (re-pattern (format "com.github.sebhoss.finj.%s-test"
                                      namespace)))))
