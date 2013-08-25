91572be083187d3e4a73c9871cb9c5ac56beb749
Sebastian Hoß <mail@shoss.de>
2013-08-25 22:11:34 +0200

    Updated license header
    Signed-off-by: Sebastian Hoß <mail@shoss.de>

--- src/main/clojure/com/github/sebhoss/finj/pension.clj
+++ src/main/clojure/com/github/sebhoss/finj/pension.clj
@@ -1,6 +1,10 @@
-;   This program is free software. It comes without any warranty, to the extent permitted by applicable law.
-;   You can redistribute it and/or modify it under the terms of the Do What The Fuck You Want To Public
-;   License, Version 2, as published by Sam Hocevar. See http://www.wtfpl.net/ for more details.
+;
+; This program is free software. It comes without any warranty, to
+; the extent permitted by applicable law. You can redistribute it
+; and/or modify it under the terms of the Do What The Fuck You Want
+; To Public License, Version 2, as published by Sam Hocevar. See
+; http://www.wtfpl.net/ for more details.
+;
 
 (ns com.github.sebhoss.finj.pension
   "A pension is a contract for a fixed sum to be paid regularly to a person, typically following retirement from
--- src/test/clojure/com/github/sebhoss/finj/ratio_test.clj
+++ src/test/clojure/com/github/sebhoss/finj/ratio_test.clj
@@ -1,3 +1,11 @@
+;
+; This program is free software. It comes without any warranty, to
+; the extent permitted by applicable law. You can redistribute it
+; and/or modify it under the terms of the Do What The Fuck You Want
+; To Public License, Version 2, as published by Sam Hocevar. See
+; http://www.wtfpl.net/ for more details.
+;
+
 (ns com.github.sebhoss.finj.ratio-test
   (:require [com.github.sebhoss.finj.ratio :refer :all]
             [com.github.sebhoss.predicate :refer :all]
