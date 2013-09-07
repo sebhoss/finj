deb87263d6d23b6c8ba2be6a23b489502a7fb085
Sebastian Hoß <mail@shoss.de>
2013-09-07 19:34:03 +0200

    Use latest bootstrap functions
    Signed-off-by: Sebastian Hoß <mail@shoss.de>

--- src/bootstrap/clojure/bootstrap.clj
+++ src/bootstrap/clojure/bootstrap.clj
@@ -6,38 +6,14 @@
 ; http://www.wtfpl.net/ for more details.
 ;
 
+(require '[bootstrap.repl :refer :all])
+
 ; Load project namespaces
-(require '(com.github.sebhoss.finj [annuity :as annuity]
-                                   [annuity-test :as annuity-test]
-                                   [common :as common]
-                                   [common-test :as common-test]
-                                   [compound-interest :as compound-interest]
-                                   [compound-interest-test :as compound-interest-test]
-                                   [deprecation :as deprecation]
-                                   [deprecation-test :as deprecation-test]
-                                   [interest :as interest]
-                                   [interest-test :as interest-test]
-                                   [investment :as investment]
-                                   [investment-test :as investment-test]
-                                   [loan :as loan]
-                                   [loan-test :as loan-test]
-                                   [pension :as pension]
-                                   [pension-test :as pension-test]
-                                   [periodic-payment :as periodic-payment]
-                                   [periodic-payment-test :as periodic-payment-test]
-                                   [ratio :as ratio]
-                                   [ratio-test :as ratio-test]
-                                   [root-finding :as root-finding]
-                                   [root-finding-test :as root-finding-test]
-                                   [share-price :as share-price]
-                                   [share-price-test :as share-price-test]))
+(load-ns-in-dir-aliased "src/main/clojure")
+(load-ns-in-dir-aliased "src/test/clojure")
 
 ; 'clojure.test', 'clojure.repl' and 'clojure.tools.namespace.repl' support
-(require '[clojure.tools.namespace.repl :refer :all])
-(require '[clojure.repl :refer :all])
-(require '[clojure.test :refer :all])
+(load-helpers)
 
-; Call (rat) to run all tests, or (rat "math") to run only tests in that namespace
-(defn rat
-  ([] (run-all-tests #"com.github.sebhoss.finj.*-test"))
-  ([namespace] (run-all-tests (re-pattern (format "com.github.sebhoss.finj.%s-test" namespace)))))
+; Call (rat) to run all tests, or (rat "loan") to run only tests in that namespace
+(def rat (test-shortcut #"com.github.sebhoss.finj.*-test"))
